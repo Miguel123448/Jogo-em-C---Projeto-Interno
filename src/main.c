357b7d9b71eb56c02dc27cf86b0a42fb1b88df0b
Breno Cruz <breno.cruz.dev@outlook.com>
2026-05-05 14:09:17 -0300

    feat: histórico, fullscreen e correção do ESC

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
 #include <string.h>
 #include "raylib.h"
+
+#define MAX_HISTORICO 100
 
 typedef enum {
     MENU,
@@ -12,125 +13,153 @@ typedef enum {
     SAIR
 } GameState;
 
-typedef struct formatoHistorico{
-    char timestamp[20]; //dependendo de como vamos fazer o tempo isso aqui pode mudar
+typedef struct {
+    char timestamp[20];
     int alvo;
     int tentativas;
     int baixos;
     int altos;
     int *palpites;
-    int capacidade; //capacidade que o ponteiro palpites tem pra armazenar.
+    int capacidade;
 } FormatoHistorico;
 
-int lerArquivo(FormatoHistorico *structHistorico);
+// ===== PROTÓTIPOS =====
+int lerArquivo(FormatoHistorico *historico);
 void salvarPartida(FormatoHistorico partida);
 FormatoHistorico iniciarPartida();
 
+// ====================== MAIN ======================
 int main() {
-    int DEBUG = 1;  //mostra o numero alvo, feito pra testes
-    srand(time(NULL));  //Necessario para a funcao gerarNumeroAleatorio
-    InitWindow(1280, 720, "Menu Raylib");
+
+    srand(time(NULL));
+    InitWindow(1280, 720, "Jogo de Adivinhacao");
+    ToggleFullscreen();
+    SetExitKey(0);
     SetTargetFPS(60);
 
     GameState estado = MENU;
-    int menuOpcaoSelecionada = 0;
-    int totalOpcoes = 3;
-    const char *opcoes[3] = {"Iniciar Jogo","Estatísticas","Sair"};
 
-    //variaveis referente a caixa de entrada do jogo
+    const char *opcoes[3] = {"Iniciar Jogo", "Estatisticas", "Sair"};
+    int opcaoSelecionada = 0;
+
+    // INPUT
     char inputTexto[50] = "";
     int inputTamanho = 0;
     bool inputAtivo = false;
     Rectangle inputBox = {100, 200, 300, 40};
-    
-    char mensagem[50] = "Digite um número";
-    Color corMsg;
+
+    char mensagem[50] = "Digite um numero";
+    Color corMsg = BLACK;
+
+    // HISTÓRICO
+    FormatoHistorico historico[MAX_HISTORICO];
+    int totalHistorico = 0;
 
     FormatoHistorico partida;
 
     while (!WindowShouldClose()) {
 
-        // ===== INPUT =====
-        if (estado == MENU) {
-            if (IsKeyPressed(KEY_DOWN)) menuOpcaoSelecionada++;
-            if (IsKeyPressed(KEY_UP)) menuOpcaoSelecionada--;
-
-            if (menuOpcaoSelecionada < 0) menuOpcaoSelecionada = totalOpcoes - 1;
-            if (menuOpcaoSelecionada >= totalOpcoes) menuOpcaoSelecionada = 0;
-
-            if (IsKeyPressed(KEY_ENTER)) {
-                if (menuOpcaoSelecionada == 0){ 
-                    estado = JOGO;
-                    inputTexto[0] = '\0';
-                    inputTamanho = 0;
-                    partida = iniciarPartida();
-                    if(DEBUG ==1) printf("%d\n", partida.alvo);
-                }
-                if (menuOpcaoSelecionada == 1) estado = ESTATISTICAS;
-                if (menuOpcaoSelecionada == 2) estado = SAIR;
+        if (IsKeyPressed(KEY_ESCAPE)) {
+            if (estado == MENU) {
+                break; // sai do jogo
+            }else {
+                estado = MENU; // volta pro menu
             }
         }
 
-        // ===== UPDATE =====
+        // ===== INPUT MENU =====
+        if (estado == MENU) {
+            if (IsKeyPressed(KEY_DOWN)) opcaoSelecionada++;
+            if (IsKeyPressed(KEY_UP)) opcaoSelecionada--;
+
+            if (opcaoSelecionada < 0) opcaoSelecionada = 2;
+            if (opcaoSelecionada > 2) opcaoSelecionada = 0;
+
+            if (IsKeyPressed(KEY_ENTER)) {
+                if (opcaoSelecionada == 0) {
+                    partida = iniciarPartida();
+                    estado = JOGO;
+
+                    inputTexto[0] = '\0';
+                    inputTamanho = 0;
+                    strcpy(mensagem, "Digite um numero");
+                }
+                else if (opcaoSelecionada == 1) {
+                    totalHistorico = lerArquivo(historico);
+                    estado = ESTATISTICAS;
+                }
+                else {
+                    estado = SAIR;
+                }
+            }
+        }
+
         if (estado == SAIR) break;
 
-        // ===== DRAW =====
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        // ===== MENU =====
         if (estado == MENU) {
 
-            DrawText("Cybercore", 300, 100, 40, GREEN);
+            DrawText("CyberCore", 500, 100, 40, DARKBLUE);
 
             for (int i = 0; i < 3; i++) {
-                Color cor = (i == menuOpcaoSelecionada) ? RED : DARKGRAY;
-                DrawText(opcoes[i], 320, 250 + i * 40, 20, cor);
+                Color cor = (i == opcaoSelecionada) ? RED : DARKGRAY;
+                DrawText(opcoes[i], 520, 250 + i * 40, 20, cor);
             }
+        }
 
-        } else if (estado == JOGO) {
+        // ===== JOGO =====
+        else if (estado == JOGO) {
+
+            // Botão voltar
             Rectangle botao = {100, 100, 250, 40};
             bool hover = CheckCollisionPointRec(GetMousePosition(), botao);
-            Color cor = hover ? RED : DARKGRAY;
-            DrawText("Voltar ao Menu", botao.x, botao.y, 30, cor);
+            DrawText("Voltar ao Menu", botao.x, botao.y, 30, hover ? RED : DARKGRAY);
+
             if (hover && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                estado = MENU;  
-        }
-        // Clique ativa/desativa
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                if (CheckCollisionPointRec(GetMousePosition(), inputBox)) {
-                    inputAtivo = true;
-                } else {
-                    inputAtivo = false;
-                }
+                estado = MENU;
             }
 
-            // Captura teclado
+            // Clique input
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                inputAtivo = CheckCollisionPointRec(GetMousePosition(), inputBox);
+            }
+
+            // Digitação
             if (inputAtivo) {
                 int tecla = GetCharPressed();
 
                 while (tecla > 0) {
-                    if (tecla >= '0' && tecla <= '9'){      //so aceita numeros
-                        if (inputTamanho < 2) {
-                            inputTexto[inputTamanho] = (char)tecla;
-                            inputTamanho++;
-                            inputTexto[inputTamanho] = '\0';
-                        }
-                }
+                    if (tecla >= '0' && tecla <= '9' && inputTamanho < 3) {
+                        inputTexto[inputTamanho++] = (char)tecla;
+                        inputTexto[inputTamanho] = '\0';
+                    }
                     tecla = GetCharPressed();
                 }
 
-                // Backspace
-                if (IsKeyPressed(KEY_BACKSPACE)) {
-                    if (inputTamanho > 0) {
-                        inputTamanho--;
-                        inputTexto[inputTamanho] = '\0';
-                    }
+                if (IsKeyPressed(KEY_BACKSPACE) && inputTamanho > 0) {
+                    inputTexto[--inputTamanho] = '\0';
                 }
-                    // Enter para confirmar
+
                 if (IsKeyPressed(KEY_ENTER) && inputTamanho > 0) {
+
                     int valor = atoi(inputTexto);
-                    partida.palpites[partida.tentativas] = valor;
-                    partida.tentativas++;
+
+                    // salva palpite
+                    if (partida.tentativas >= partida.capacidade) {
+                        partida.capacidade *= 2;
+
+                        int *temp = realloc(partida.palpites, partida.capacidade * sizeof(int));
+                        if (!temp) exit(1);
+
+                        partida.palpites = temp;
+                    }
+
+                    partida.palpites[partida.tentativas++] = valor;
+
+                    // lógica do jogo
                     if (valor > partida.alvo) {
                         strcpy(mensagem, "Muito alto!");
                         corMsg = RED;
@@ -144,151 +173,146 @@ int main() {
                     else {
                         strcpy(mensagem, "Acertou!");
                         corMsg = GREEN;
+
                         salvarPartida(partida);
-                        estado = MENU;  //se acertar volta para o menu, temporario
+
+                        free(partida.palpites);
+                        estado = MENU;
                     }
-                    if (partida.tentativas == partida.capacidade) {
-                        partida.capacidade *= 2; // dobra
-                        partida.palpites = realloc(partida.palpites,partida.capacidade * sizeof(int));
-                        if(partida.palpites == NULL){
-                            printf("Erro de memoria, saindo do jogo...");
-                            exit(1);
-                        }
-                    }
-                    // limpa input
-                    inputTamanho = 0;
+
                     inputTexto[0] = '\0';
+                    inputTamanho = 0;
                 }
             }
-                DrawText(mensagem, inputBox.x, inputBox.y - 40, 20, corMsg);    //mensagem de "alto/baixo/acerto"
-                // Caixa
-                DrawRectangleRec(inputBox, LIGHTGRAY);
-                DrawRectangleLinesEx(inputBox, 2, inputAtivo ? BLUE : DARKGRAY);
 
-                // Texto digitado
-                DrawText(inputTexto, inputBox.x + 10, inputBox.y + 10, 20, BLACK);
+            DrawText(mensagem, inputBox.x, inputBox.y - 40, 20, corMsg);
 
-                // Placeholder
-                if (inputTamanho == 0 && !inputAtivo) {
-                    DrawText("Digite um número...", inputBox.x + 10, inputBox.y + 10, 20, GRAY);
+            DrawRectangleRec(inputBox, LIGHTGRAY);
+            DrawRectangleLinesEx(inputBox, 2, inputAtivo ? BLUE : DARKGRAY);
+
+            DrawText(inputTexto, inputBox.x + 10, inputBox.y + 10, 20, BLACK);
+        }
+
+        // ===== ESTATISTICAS =====
+        else if (estado == ESTATISTICAS) {
+
+            DrawText("ESTATISTICAS", 450, 50, 30, DARKBLUE);
+
+            if (totalHistorico <= 0) {
+                DrawText("Nenhum historico encontrado.", 400, 200, 20, RED);
+            }
+            else {
+                int y = 120;
+
+                for (int i = 0; i < totalHistorico && i < 10; i++) {
+
+                    char linha[300];
+
+                    sprintf(linha,
+                        "%s | Alvo: %d | Tentativas: %d | Altos: %d | Baixos: %d",
+                        historico[i].timestamp,
+                        historico[i].alvo,
+                        historico[i].tentativas,
+                        historico[i].altos,
+                        historico[i].baixos
+                    );
+
+                    DrawText(linha, 100, y, 18, BLACK);
+                    y += 30;
                 }
+            }
 
-                // Cursor piscando
-                if (inputAtivo && ((int)(GetTime() * 2) % 2 == 0)) {
-                    int larguraTexto = MeasureText(inputTexto, 20);
-                    DrawText("|",inputBox.x + 10 + larguraTexto, inputBox.y + 10,20,BLACK);
-                }
-    }
+            DrawText("Pressione ESC para voltar", 400, 650, 20, GRAY);
+
+            if (IsKeyPressed(KEY_ESCAPE)) {
+                estado = MENU;
+            }
+        }
 
         EndDrawing();
-    } 
-
-    /*for (int i = 0; i < total; i++) {
-        free(structHistorico[i].palpites);
     }
-       LEMBRAR DE DAR FREE NOS PONTEIROS NO FINAL DO CODIGO 
-    */
+
+    // liberar memória
+    for (int i = 0; i < totalHistorico; i++) {
+        free(historico[i].palpites);
+    }
+
     CloseWindow();
     return 0;
 }
 
-FormatoHistorico iniciarPartida(){
-    FormatoHistorico partida;
-    //timestamp
-    time_t t = time(NULL);
-    struct tm *tm_info = localtime(&t);
-    strftime(partida.timestamp, 20, "%Y-%m-%d %H:%M", tm_info);
-    //dados do jogos
-    partida.alvo = (rand() % 100 + 1);
-    partida.tentativas = 0;
-    partida.altos = 0;
-    partida.baixos = 0;
-    //alocacao de memoria
-    partida.capacidade = 10;
-    partida.palpites = malloc(partida.capacidade * sizeof(int));
+// ================= FUNÇÕES =================
 
-    if (partida.palpites == NULL) {
-        printf("Erro ao alocar memória!\n");
-        exit(1);
-    }
-    return partida;
+FormatoHistorico iniciarPartida() {
+
+    FormatoHistorico p;
+
+    time_t t = time(NULL);
+    strftime(p.timestamp, 20, "%Y-%m-%d %H:%M", localtime(&t));
+
+    p.alvo = rand() % 100 + 1;
+    p.tentativas = 0;
+    p.altos = 0;
+    p.baixos = 0;
+
+    p.capacidade = 10;
+    p.palpites = malloc(p.capacidade * sizeof(int));
+
+    if (!p.palpites) exit(1);
+
+    return p;
 }
 
+void salvarPartida(FormatoHistorico p) {
 
-int lerArquivo(FormatoHistorico *structHistorico){
+    FILE *f = fopen("historico.txt", "a");
+    if (!f) return;
 
-    FILE *arquivo = fopen("historico.txt","r");
+    fprintf(f, "%s;%d;%d;%d;%d;",
+            p.timestamp, p.alvo, p.tentativas, p.baixos, p.altos);
 
-    if(arquivo == NULL){
-        return -1;
+    for (int i = 0; i < p.tentativas; i++) {
+        fprintf(f, "%d", p.palpites[i]);
+        if (i < p.tentativas - 1) fprintf(f, ",");
     }
+
+    fprintf(f, "\n");
+    fclose(f);
+}
+
+int lerArquivo(FormatoHistorico *h) {
+
+    FILE *f = fopen("historico.txt", "r");
+    if (!f) return 0;
 
     char linha[256];
     int i = 0;
 
-    while(fgets(linha, sizeof(linha), arquivo) != NULL){
+    while (fgets(linha, sizeof(linha), f) && i < MAX_HISTORICO) {
 
         char palpitesStr[200];
 
-        // faz o parse da linha
-        if(sscanf(linha, "%19[^;];%d;%d;%d;%d;%[^\n]",
-               structHistorico[i].timestamp,
-               &structHistorico[i].alvo,
-               &structHistorico[i].tentativas,
-               &structHistorico[i].baixos,
-               &structHistorico[i].altos,
-               palpitesStr) != 6){
-            continue; // pula linha inválida
-        }
+        if (sscanf(linha, "%19[^;];%d;%d;%d;%d;%[^\n]",
+            h[i].timestamp,
+            &h[i].alvo,
+            &h[i].tentativas,
+            &h[i].baixos,
+            &h[i].altos,
+            palpitesStr) != 6) continue;
 
-        // aloca memória para os palpites
-        structHistorico[i].palpites = malloc(structHistorico[i].tentativas * sizeof(int));
-        if (structHistorico[i].palpites == NULL) {
-            printf("Erro de memória\n");
-            break;
-        }
+        h[i].palpites = malloc(h[i].tentativas * sizeof(int));
 
-        // separa CSV dos palpites
         char *token = strtok(palpitesStr, ",");
         int j = 0;
 
-        while(token != NULL && j < structHistorico[i].tentativas){
-            structHistorico[i].palpites[j]= atoi(token);
-            token = strtok(NULL, ","); 
-            j++;
+        while (token && j < h[i].tentativas) {
+            h[i].palpites[j++] = atoi(token);
+            token = strtok(NULL, ",");
         }
 
         i++;
     }
 
-    fclose(arquivo);
-    return i; // quantidade de linhas lidas
-}
-
-void salvarPartida(FormatoHistorico partida){
-
-    FILE *arquivo = fopen("historico.txt", "a");
-
-    if(arquivo == NULL){
-        printf("Erro ao abrir arquivo!\n");
-        return;
-    }
-
-    fprintf(arquivo, "%s;%d;%d;%d;%d;",
-            partida.timestamp,
-            partida.alvo,
-            partida.tentativas,
-            partida.baixos,
-            partida.altos);
-
-    for(int i = 0; i < partida.tentativas; i++){
-        fprintf(arquivo, "%d", partida.palpites[i]);
-        if(i < partida.tentativas - 1){
-            fprintf(arquivo, ",");
-        }
-    }
-
-    fprintf(arquivo, "\n");
-
-    fclose(arquivo);
+    fclose(f);
+    return i;
 }
